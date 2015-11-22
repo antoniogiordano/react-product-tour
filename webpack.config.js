3731f7e806d421a2e546181bade3fffae751d917
antoniogiordano <antonio.giordano@getapper.com>
2015-11-22 22:01:36 +0100

    Reorganized Dist and Examples folders

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var path = require('path')
 module.exports = {
   entry: './app.js',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'examples'),
     filename: 'bundle.js'
   },
   module: {
